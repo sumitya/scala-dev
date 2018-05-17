2978790ddf68b7fad5915e3bd1942b683bb17de7
sumitya <syadavgit@gmail.com>
2018-05-17 10:13:16 +0530

    adding examples of scala methods. Methods with primitive argument and method with function as arguments.

---
+++ src/main/scala/basics/Methods.scala
@@ -0,0 +1,31 @@
+package basics
+
+object Methods {
+
+  //simple method
+  def calculation(a: Int, b: Int):Unit = {
+    println(a / b)
+  }
+
+  //method with function as argument
+  def calcWithMethod(a: String,fx:String => String) : String = {
+
+    fx(a).toString
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    calculation(3,3)
+
+    val fx = (a:String) => a.toUpperCase
+
+
+    val result = calcWithMethod("example",fx)
+
+    val sameresult = calcWithMethod("example",(a:String) => a.toUpperCase)
+
+    println(result)
+
+    println(sameresult)
+  }
+}
