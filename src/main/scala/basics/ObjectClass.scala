2e2570ff883854479ba71175e8ac72c38c46501b
sumitya <syadavgit@gmail.com>
2018-05-19 12:01:32 +0530

    modified examples of scala methods, adding class, case class, object class and traits example.

---
+++ src/main/scala/basics/ObjectClass.scala
@@ -0,0 +1,35 @@
+package basics
+
+object ObjectClass {
+
+  def main(args: Array[String]): Unit = {
+
+    //basics.Classes call
+    val obj = new Classes()
+    println(obj.printer("Printer"))
+
+    //basics.CaseClasses call
+    val caseobj = CaseClasses(1,2)
+    println(caseobj.a)
+
+    //basics.Eagle call
+    val eagle = new Eagle()
+    println(eagle.color)
+    eagle.fly()
+
+  }
+
+  /* Object class's main method is the entry point for any program execution.
+
+  javap -p basics/ObjectClass.class
+
+  Compiled from "ObjectClass.scala"
+
+    public final class basics.ObjectClass {
+      public static void main(java.lang.String[]);
+      ...................
+      ........
+    }
+  */
+
+}
